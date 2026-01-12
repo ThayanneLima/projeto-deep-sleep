7705681e93c20c881d353abad138c780b5fae19a
Thayanne <thayannecflima@gmail.com>
2026-01-12 10:12:41 -0300

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-//importação de biblioteca - Versão de envio com sucesso
+// importação de biblioteca - Versão de envio com sucesso
 #include <Arduino.h> //caso não utilizar Arduino IDE
 #include <lmic.h>
 #include <Wire.h>
@@ -16,6 +16,8 @@
 #define OLED_RST 16
 #define OLED_ADDR 0x3C
 
+#define LORA_GAIN 20
+
 // Tempo de deep sleep em microssegundos (15 segundos)
 #define SLEEP_TIME 15000000
 
@@ -25,31 +27,36 @@ Adafruit_INA219 ina219(0x40);
 // payload para envio
 uint8_t payload[10];
 
-//PROTÓTIPOS DE FUNÇÕES
+uint8_t txBuffer[10];
+int n_packet = 0;
+
+// PROTÓTIPOS DE FUNÇÕES
 void sensors_build_payload();
 void sensors_setup();
 
-//configuração de padrão de segurança
-//descomentar a configuração desejada
-#define USE_OTAA
-//#define USE_ABP 
+#define CFG_au915
 
-#ifdef USE_ABP //dados atualizados
-  static const PROGMEM u1_t NWKSKEY[16] = { 0xD2, 0xAF, 0x51, 0xA7, 0xE5, 0x1B, 0x8E, 0xC8, 0xA6, 0x0D, 0x43, 0x84, 0xCE, 0x48, 0xFD, 0x3A }; //msb format
-  static const u1_t PROGMEM APPSKEY[16] = { 0xF5, 0x9D, 0x4E, 0x58, 0x41, 0x51, 0x1C, 0x36, 0xAE, 0x04, 0xF9, 0xCB, 0x77, 0x6B, 0x54, 0x82 }; //msb format
-  static const u4_t DEVADDR = 0x260DB618;
-  void os_getArtEui (u1_t* buf) { }
-  void os_getDevEui (u1_t* buf) { }
-  void os_getDevKey (u1_t* buf) { }
+// configuração de padrão de segurança
+// descomentar a configuração desejada
+// #define USE_OTAA
+#define USE_ABP
+
+#ifdef USE_ABP                                                                                                                            // dados atualizados
+static const PROGMEM u1_t NWKSKEY[16] = {0xD0, 0x1F, 0xDF, 0x26, 0x49, 0x28, 0xEA, 0xD4, 0x50, 0x67, 0x01, 0x27, 0x50, 0x49, 0x92, 0x25}; // msb format
+static const u1_t PROGMEM APPSKEY[16] = {0x74, 0x18, 0xC3, 0x17, 0x06, 0x8C, 0x9D, 0xC4, 0xE3, 0xAB, 0x0D, 0x36, 0xD3, 0x89, 0x12, 0xAF}; // msb format
+static const u4_t DEVADDR = 0x260D11DF;
+void os_getArtEui(u1_t *buf) {}
+void os_getDevEui(u1_t *buf) {}
+void os_getDevKey(u1_t *buf) {}
 #endif
 
 #ifdef USE_OTAA
-  static const u1_t PROGMEM APPEUI[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; //lsb format
-  void os_getArtEui(u1_t *buf) { memcpy_P(buf, APPEUI, 8); }
-  static const u1_t PROGMEM DEVEUI[8]  = { 0xAA, 0x4D, 0x07, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 }; // lsb format
-  void os_getDevEui(u1_t *buf) { memcpy_P(buf, DEVEUI, 8); }
-  static const u1_t PROGMEM APPKEY[16] = { 0xFD, 0xE1, 0x58, 0x5F, 0x53, 0x5E, 0x1D, 0xDC, 0x79, 0x47, 0x7E, 0xA5, 0xA2, 0x02, 0xBD, 0x0C }; //msb format
-  void os_getDevKey(u1_t *buf) { memcpy_P(buf, APPKEY, 16); }
+static const u1_t PROGMEM APPEUI[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // lsb format
+void os_getArtEui(u1_t *buf) { memcpy_P(buf, APPEUI, 8); }
+static const u1_t PROGMEM DEVEUI[8] = {0xAA, 0x4D, 0x07, 0xD0, 0x7E, 0xD5, 0xB3, 0x70}; // lsb format
+void os_getDevEui(u1_t *buf) { memcpy_P(buf, DEVEUI, 8); }
+static const u1_t PROGMEM APPKEY[16] = {0xFD, 0xE1, 0x58, 0x5F, 0x53, 0x5E, 0x1D, 0xDC, 0x79, 0x47, 0x7E, 0xA5, 0xA2, 0x02, 0xBD, 0x0C}; // msb format
+void os_getDevKey(u1_t *buf) { memcpy_P(buf, APPKEY, 16); }
 #endif
 
 enum OperationMode
@@ -60,6 +67,14 @@ enum OperationMode
   MODE_DEEP_SLEEP,  // 3.5V to 3.7V
   MODE_HIBERNATION  // less than 3.5V
 };
+
+/*
+0 → ACTIVE
+1 → MODEM_SLEEP
+2 → LIGHT_SLEEP
+3 → DEEP_SLEEP
+4 → HIBERNATION
+*/
 
 OperationMode operationMode;
 
@@ -72,18 +87,20 @@ Adafruit_BMP280 bmp;
 unsigned long lastMsg = 0;
 float temperatura = 0;
 float pressao = 0;
+float energy_Wh = 0.0;
+unsigned long lastEnergyMs = 0;
 
-//objeto que será mandado para a função do_send()
+// objeto que será mandado para a função do_send()
 static osjob_t sendjob;
 
-//intervalo de envio
+// intervalo de envio
 const unsigned TX_INTERVAL = 20;
 
 // Mapa de pinos Heltec ESP32Lora v2
 const lmic_pinmap lmic_pins = {
     .nss = 18,
     .rxtx = LMIC_UNUSED_PIN,
-    .rst = 14, 
+    .rst = 14,
     .dio = {26, 35, 34},
 };
 
@@ -94,85 +111,85 @@ void do_send(osjob_t *j);
 // comunicação com o gateway LoRaWAN.
 void onEvent(ev_t ev)
 {
-    Serial.print(os_getTime());
-    Serial.print(": ");
-    switch (ev)
+  Serial.print(os_getTime());
+  Serial.print(": ");
+  switch (ev)
+  {
+  case EV_SCAN_TIMEOUT:
+    Serial.println(F("EV_SCAN_TIMEOUT"));
+    break;
+  case EV_BEACON_FOUND:
+    Serial.println(F("EV_BEACON_FOUND"));
+    break;
+  case EV_BEACON_MISSED:
+    Serial.println(F("EV_BEACON_MISSED"));
+    break;
+  case EV_BEACON_TRACKED:
+    Serial.println(F("EV_BEACON_TRACKED"));
+    break;
+  case EV_JOINING:
+    Serial.println(F("EV_JOINING"));
+    break;
+  case EV_JOINED:
+    Serial.println(F("EV_JOINED"));
+    break;
+  case EV_JOIN_FAILED:
+    Serial.println(F("EV_JOIN_FAILED"));
+    LMIC_setLinkCheckMode(0);
+    break;
+  case EV_REJOIN_FAILED:
+    Serial.println(F("EV_REJOIN_FAILED"));
+    break;
+  case EV_TXCOMPLETE:
+    Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+    if (LMIC.txrxFlags & TXRX_ACK)
+      Serial.println(F("Received ack"));
+    if (LMIC.dataLen)
     {
-    case EV_SCAN_TIMEOUT:
-        Serial.println(F("EV_SCAN_TIMEOUT"));
-        break;
-    case EV_BEACON_FOUND:
-        Serial.println(F("EV_BEACON_FOUND"));
-        break;
-    case EV_BEACON_MISSED:
-        Serial.println(F("EV_BEACON_MISSED"));
-        break;
-    case EV_BEACON_TRACKED:
-        Serial.println(F("EV_BEACON_TRACKED"));
-        break;
-    case EV_JOINING:
-        Serial.println(F("EV_JOINING"));
-        break;
-    case EV_JOINED:
-        Serial.println(F("EV_JOINED"));
-        break;
-    case EV_JOIN_FAILED:
-        Serial.println(F("EV_JOIN_FAILED"));
-        LMIC_setLinkCheckMode(0);
-        break;
-    case EV_REJOIN_FAILED:
-        Serial.println(F("EV_REJOIN_FAILED"));
-        break;
-    case EV_TXCOMPLETE:
-        Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-        if (LMIC.txrxFlags & TXRX_ACK)
-            Serial.println(F("Received ack"));
-        if (LMIC.dataLen)
-        {
-            Serial.print(F("Received "));
-            Serial.print(LMIC.dataLen);
-            Serial.println(F(" bytes of payload"));
-        }
-        if (LMIC.dataLen == 1) 
-        {
-            uint8_t dados_recebidos = LMIC.frame[LMIC.dataBeg + 0];
-            Serial.print(F("Dados recebidos: "));
-            Serial.write(dados_recebidos);
-        }
-        // Agenda a transmissão automática com intervalo de TX_INTERVAL
-        os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
-        break;
-    case EV_LOST_TSYNC:
-        Serial.println(F("EV_LOST_TSYNC"));
-        break;
-    case EV_RESET:
-        Serial.println(F("EV_RESET"));
-        break;
-    case EV_RXCOMPLETE:
-        Serial.println(F("EV_RXCOMPLETE"));
-        break;
-    case EV_LINK_DEAD:
-        Serial.println(F("EV_LINK_DEAD"));
-        break;
-    case EV_LINK_ALIVE:
-        Serial.println(F("EV_LINK_ALIVE"));
-        break;
-    case EV_TXSTART:
-        Serial.println(F("EV_TXSTART"));
-        break;
-    case EV_TXCANCELED:
-        Serial.println(F("EV_TXCANCELED"));
-        break;
-    case EV_RXSTART:
-        break;
-    case EV_JOIN_TXCOMPLETE:
-        Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
-        break;
-    default:
-        Serial.print(F("Unknown event: "));
-        Serial.println((unsigned)ev);
-        break;
+      Serial.print(F("Received "));
+      Serial.print(LMIC.dataLen);
+      Serial.println(F(" bytes of payload"));
     }
+    if (LMIC.dataLen == 1)
+    {
+      uint8_t dados_recebidos = LMIC.frame[LMIC.dataBeg + 0];
+      Serial.print(F("Dados recebidos: "));
+      Serial.write(dados_recebidos);
+    }
+    // Agenda a transmissão automática com intervalo de TX_INTERVAL
+    os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
+    break;
+  case EV_LOST_TSYNC:
+    Serial.println(F("EV_LOST_TSYNC"));
+    break;
+  case EV_RESET:
+    Serial.println(F("EV_RESET"));
+    break;
+  case EV_RXCOMPLETE:
+    Serial.println(F("EV_RXCOMPLETE"));
+    break;
+  case EV_LINK_DEAD:
+    Serial.println(F("EV_LINK_DEAD"));
+    break;
+  case EV_LINK_ALIVE:
+    Serial.println(F("EV_LINK_ALIVE"));
+    break;
+  case EV_TXSTART:
+    Serial.println(F("EV_TXSTART"));
+    break;
+  case EV_TXCANCELED:
+    Serial.println(F("EV_TXCANCELED"));
+    break;
+  case EV_RXSTART:
+    break;
+  case EV_JOIN_TXCOMPLETE:
+    Serial.println(F("EV_JOIN_TXCOMPLETE: no JoinAccept"));
+    break;
+  default:
+    Serial.print(F("Unknown event: "));
+    Serial.println((unsigned)ev);
+    break;
+  }
 }
 
 void sensors_setup()
@@ -182,8 +199,16 @@ void sensors_setup()
   // Temperatura e pressão representativas do bloco de código BMP280
   temperatura = bmp.readTemperature();   // °C
   pressao = bmp.readPressure() / 100.0F; // hPa
-  
-  /* Operation mode
+
+  // Cálculo de energia acumulada
+  float v = batteryVoltage;          // V
+  float i = batteryCurrent / 1000.0; // A  (porque vem em mA)
+  float p = v * i;                   // W
+
+  energy_Wh += p * (TX_INTERVAL / 3600.0); // Wh
+  Serial.printf("Energia acumulada: %.5f Wh\n", energy_Wh);
+
+  // Definição do modo de operação baseado na tensão da bateria
   if (batteryVoltage >= 4.0)
   {
     operationMode = MODE_ACTIVE;
@@ -204,8 +229,8 @@ void sensors_setup()
   {
     operationMode = MODE_HIBERNATION;
   }
-*/
-  operationMode = MODE_ACTIVE;
+
+  // operationMode = MODE_ACTIVE;
   display.clearDisplay();
 
   switch (operationMode)
@@ -253,9 +278,9 @@ void sensors_setup()
   display.setCursor(0, 0);
   display.printf("Bateria:");
   display.setCursor(0, 10);
-  display.printf("%.1fmA", batteryCurrent);
-  display.setCursor(0, 20);
   display.printf("%.1fV", batteryVoltage);
+  display.setCursor(0, 20);
+  display.printf("%.1fmA", batteryCurrent);
   display.display();
 
   Serial.printf("Tensão da bateria: %.2f V\n", batteryVoltage);
@@ -264,52 +289,162 @@ void sensors_setup()
   Serial.printf("Pressao: %.2f hPa\n", pressao);
 }
 
-void sensors_build_payload()
+void sensors_build_payload(uint8_t payload[10])
 {
   sensors_setup();
   int16_t voltage_mv = static_cast<int16_t>(batteryVoltage * 1000);
-  int16_t current_ma = static_cast<int16_t>(batteryCurrent);
-  int16_t temperature_c = static_cast<int16_t>(temperatura * 100);
-  int32_t pressure_pa = static_cast<int32_t>(pressao * 100);
+  int16_t current_ma = static_cast<int16_t>(batteryCurrent * 100);
+  // int16_t temperature_c = static_cast<int16_t>(temperatura * 100);
+  // int32_t pressure_pa = static_cast<int32_t>(pressao * 100);
+  // uint16_t energy_x100 = (uint16_t)(energy_Wh * 100.0); // Wh com 2 casas
+  uint32_t energy_mWh = (uint32_t)(energy_Wh * 1000000.0); // mWh
 
   payload[0] = (voltage_mv >> 8) & 0xFF;
   payload[1] = voltage_mv & 0xFF;
   payload[2] = (current_ma >> 8) & 0xFF;
   payload[3] = current_ma & 0xFF;
-  payload[4] = (temperature_c >> 8) & 0xFF;
-  payload[5] = temperature_c & 0xFF;
-  payload[6] = (pressure_pa >> 16) & 0xFF;
-  payload[7] = (pressure_pa >> 8) & 0xFF;
-  payload[8] = pressure_pa & 0xFF;
+  // payload[4] = (temperature_c >> 8) & 0xFF;
+  // payload[5] = temperature_c & 0xFF;
+  // payload[6] = (pressure_pa >> 16) & 0xFF;
+  // payload[7] = (pressure_pa >> 8) & 0xFF;
+  // payload[8] = pressure_pa & 0xFF;
+  payload[4] = (energy_mWh >> 24) & 0xFF;
+  payload[5] = (energy_mWh >> 16) & 0xFF;
+  payload[6] = (energy_mWh >> 8) & 0xFF;
+  payload[7] = energy_mWh & 0xFF;
+
+  payload[8] = (uint8_t)(operationMode);
+
+  /* Preenche os bytes restantes com zeros (ou use-os para outros dados, se necessário)
+  for (int i = 9; i < 12; i++) {
+    txBuffer[i] = 0;
+  }*/
 }
 
 void do_send(osjob_t *j)
 {
-    //static uint8_t payload[] = "Hello, world!";
-    sensors_build_payload();
-    // Verifica se não está ocorrendo uma transmissão no momento TX/RX
-    if (LMIC.opmode & OP_TXRXPEND){
-        Serial.println(F("OP_TXRXPEND, not sending"));
+  // static uint8_t payload[] = "Hello, world!";
+
+  // Verifica se não está ocorrendo uma transmissão no momento TX/RX
+  if (LMIC.opmode & OP_TXRXPEND)
+  {
+    Serial.println(F("OP_TXRXPEND, not sending"));
+  }
+  else
+  {
+    // envio
+    // LMIC_setTxData2(1, payload, sizeof(payload) - 1, 0);
+    // Serial.println(F("Sended"));
+    sensors_build_payload(payload);
+    Serial.print("TX bytes: ");
+    for (int k = 0; k < 10; k++)
+    {
+      Serial.printf("%02X ", payload[k]);
     }
-    else{
-        //envio
-        LMIC_setTxData2(1, payload, sizeof(payload)-1, 0);
-        Serial.println(F("Sended"));
-    }
+    Serial.println();
+
+    // n_packet++;
+    LMIC_setTxData2(1, payload, sizeof(payload), 0); // é responsável por enviar os dados no uplink para o servidor LoRaWAN
+    Serial.println(F("Packet queued"));
+  }
 }
 
 void setup()
 {
+
+  // inicialização da serial
+  Serial.begin(115200);
+  Serial.println(F("Iniciando..."));
+
+  // LMIC init
+  os_init();
+  // Reset the MAC state. Session and pending data transfers will be discarded.
+  LMIC_reset();
+
+  // aqui começa a mudar
 #ifdef USE_ABP
   uint8_t appskey[sizeof(APPSKEY)];
   uint8_t nwkskey[sizeof(NWKSKEY)];
   memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
   memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
   LMIC_setSession(0x13, DEVADDR, nwkskey, appskey);
+
+// aqui começa a mudar
+#else
+  // If not running an AVR with PROGMEM, just use the arrays directly
+  LMIC_setSession(0x13, DEVADDR, NWKSKEY, APPSKEY);
 #endif
 
-  Serial.begin(115200);
-  Serial.println(F("Starting"));
+#if defined(CFG_eu868)
+  // Set up the channels used by the Things Network, which corresponds
+  // to the defaults of most gateways. Without this, only three base
+  // channels from the LoRaWAN specification are used, which certainly
+  // works, so it is good for debugging, but can overload those
+  // frequencies, so be sure to configure the full frequency range of
+  // your network here (unless your network autoconfigures them).
+  // Setting up channels should happen after LMIC_setSession, as that
+  // configures the minimal channel set. The LMIC doesn't let you change
+  // the three basic settings, but we show them here.
+  LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
+  LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI); // g-band
+  LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
+  LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
+  LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
+  LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
+  LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
+  LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7), BAND_CENTI);  // g-band
+  LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK, DR_FSK), BAND_MILLI);   // g2-band
+                                                                               // LMIC_setupChannel(8, 994000000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  2);      // g2-band
+  // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+  // devices' ping slots. LMIC does not have an easy way to define set this
+  // frequency and support for class B is spotty and untested, so this
+  // frequency is not configured here.
+#elif defined(CFG_us915) || defined(CFG_au915)
+  // NA-US and AU channels 0-71 are configured automatically
+  // but only one group of 8 should (a subband) should be active
+  // TTN recommends the second sub band, 1 in a zero based count.
+  // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+  LMIC_selectSubBand(1);
+
+#elif defined(CFG_as923)
+  // Set up the channels used in your country. Only two are defined by default,
+  // and they cannot be changed.  Use BAND_CENTI to indicate 1% duty cycle.
+  // LMIC_setupChannel(0, 923200000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
+  // LMIC_setupChannel(1, 923400000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);
+
+  // ... extra definitions for channels 2..n here
+#elif defined(CFG_kr920)
+  // Set up the channels used in your country. Three are defined by default,
+  // and they cannot be changed. Duty cycle doesn't matter, but is conventionally
+  // BAND_MILLI.
+  // LMIC_setupChannel(0, 922100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(1, 922300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(2, 922500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+
+  // ... extra definitions for channels 3..n here.
+#elif defined(CFG_in866)
+  // Set up the channels used in your country. Three are defined by default,
+  // and they cannot be changed. Duty cycle doesn't matter, but is conventionally
+  // BAND_MILLI.
+  // LMIC_setupChannel(0, 865062500, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(1, 865402500, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+  // LMIC_setupChannel(2, 865985000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_MILLI);
+
+  // ... extra definitions for channels 3..n here.
+#else
+#error Region not supported
+#endif
+
+  // Disable link check validation
+  LMIC_setLinkCheckMode(0);
+
+  // TTN uses SF9 for its RX2 window.
+  LMIC.dn2Dr = DR_SF10;
+
+  // Set data rate and transmit power for uplink
+  LMIC_setDrTxpow(DR_SF12, LORA_GAIN);
+  // aqui termina a mudança
+  // #endif
 
   // Display
   pinMode(OLED_RST, OUTPUT);
@@ -344,8 +479,6 @@ void setup()
   // INA219_CONFIG_SADCRES_12BIT_128S
   //);
 
-  operationMode = MODE_ACTIVE;
-
   // Inicia BMP280
   if (!bmp.begin(0x76))
   { // ou 0x77 dependendo do seu sensor
@@ -354,20 +487,15 @@ void setup()
     display.setCursor(0, 0);
     display.println("Erro BMP280!");
     display.display();
-    while (true);
+    while (true)
+      ;
   }
 
   Serial.println("Sensor BMP280 iniciado.");
 
   sensors_setup();
 
-  // LMIC init
-  os_init();
-  // Reset the MAC state. Session and pending data transfers will be discarded.
-  LMIC_reset();
-
   do_send(&sendjob); // Start
-  
 }
 
 void loop()
